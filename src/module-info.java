a6519a4d716e60b73e816b43537ae25258dc99a2
Rodrigodx <rodrigodx52@gmail.com>
2023-03-29 11:17:02 -0300

    Department Service

--- src/module-info.java
+++ src/module-info.java
@@ -2,8 +2,12 @@ module workshop_javafx {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
-	
-	opens application to javafx.graphics, javafx.fxml;
-	
-	opens gui to javafx.graphics, javafx.fxml;
+
+       opens application to javafx.graphics, javafx.fxml;
+
+       opens gui to javafx.graphics, javafx.fxml;
+
+       opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
+
+       opens model.services to javafx.graphics, javafx.fxml;
 }
